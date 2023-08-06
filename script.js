3400ca5421c5edb4d5e5d5eedabf354bff3f924c
séverine zapata <severine.zapata@orange.fr>
2023-08-06 11:57:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+    var nom = prompt("comment t'appelles-tu ?");
+    var calculation = Number (prompt("Quel âge as-tu ?"));
+    calculation = calculation + 1;
+    document.write("Tu t'appelles " + nom + ", quand tu auras " + calculation + " ans, tu seras concepteur-développeur !")
